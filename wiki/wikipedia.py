2c190ad823f706a73613cc2568feab4a7bb17fd5
Vishal Naik <59202862+Monsieurvishal@users.noreply.github.com>
2020-04-09 15:50:07 +0530

    Update wikipedia.py
    #read data from Wikipedia and store the summary in a fileand store

--- wiki/wikipedia.py
+++ wiki/wikipedia.py
@@ -1,1 +1,13 @@
+#read data from Wikipedia and store the summary in a fileand store
 
+import wikipedia #If you do nt have wikipedia pip install wikipedia
+
+result = wikipedia.page("Dhoni") 
+
+print(result.summary) 
+
+file1 = open("myfile.txt","w") 
+
+file1.write(result.summary)
+
+file1.close()
